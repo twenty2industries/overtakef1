52cbe11a6cefbcd9fcf96fff8167e3508da90cb7
twenty2industries <burak.bostanli.1@web.de>
2025-08-11 16:48:39 +0200

    Refactor driver standings to use async data service
    Replaced static data in driver standings component with observables from a new StandingsDataService. Added loading spinner and improved UI responsiveness using Angular's @defer and async pipe. Updated styles and HTML structure for better layout and loading experience. Preloaded background images in index.html for performance.

--- src/app/components/driver-standings/driver-standings.component.ts
+++ src/app/components/driver-standings/driver-standings.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Signal } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
 import { Standing } from '../../shared/interfaces/driver.interface';
 import { Constructor } from '../../shared/interfaces/constructor.interface';
 import standings from '../../../data/standings.json';
 import constructor from '../../../data/constructor.json';
 import { trigger, transition, style, animate } from '@angular/animations';
+import { StandingsDataService } from '../../shared/services/standings-data.service';
+import { combineLatest, map, Observable, startWith, delay } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-driver-standings',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './driver-standings.component.html',
   styleUrl: './driver-standings.component.scss',
   animations: [
@@ -29,15 +33,28 @@ import { trigger, transition, style, animate } from '@angular/animations';
   ],
 })
 export class DriverStandingsComponent {
-
   openMenu: string = 'drivers-standing';
 
-  driverStandings: Standing[] = standings;
-  
-  constructosStandings: Constructor[] = constructor;
+  drivers$!: Observable<Standing[]>;
+  constructors$!: Observable<Constructor[]>;
+  loaded!: Signal<boolean>;
 
-  constructor() {
-    this.driverStandings = standings.sort((a, b) => b.points - a.points);
-    this.constructosStandings = constructor.sort((a, b) => b.points - a.points);
+  constructor(private standingsDataService: StandingsDataService) {
+    this.drivers$ = this.standingsDataService.getDriverStandings$();
+    this.constructors$ = this.standingsDataService.getConstructorStandings$();
+this.loaded = toSignal(
+  combineLatest([this.drivers$, this.constructors$]).pipe(
+    map(() => true),
+    delay(1500) // Ladezeit simulieren
+  ),
+  { initialValue: false }
+);
+/* 
+    this.loaded = toSignal(
+      combineLatest([this.drivers$, this.constructors$]).pipe(map(() => true)),
+      { initialValue: false }
+    ); */
   }
+
+  
 }
---
+++ src/app/shared/services/standings-data.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Standing } from '../interfaces/driver.interface';
+import { Constructor } from '../interfaces//constructor.interface';
+
+import standings from '../../../data/standings.json';
+import constructor from '../../../data/constructor.json';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StandingsDataService {
+  getDriverStandings$(): Observable<Standing[]> { // The $ suffix is an angular/ts convention indicating that the value is an observable that should be subscribed to or consumed using async in the template
+    return of(standings).pipe(map(a => [...a].sort((x,y)=>y.points-x.points)));
+  }
+
+    getConstructorStandings$(): Observable<Constructor[]> {
+    return of(constructor).pipe(map(a => [...a].sort((x,y)=>y.points-x.points)));
+  }
+
+  constructor() { }
+}
